161427693af49576edc9aca5367cc0b0971a32e8
Chandan <chandan.raikar@live.com>
2026-09-16 16:57:24 +0530

    Created a global context

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import AuthProvider from './components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { GlobalProvider } from './context/GlobalContext';
 
 export const metadata = {
     title: {
@@ -17,16 +18,18 @@ export const metadata = {
 const MainLayout = ({ children }) => {
     return (
         <AuthProvider>
-            <html>
-                <body>
-                    <Navbar></Navbar>
-                    <main>
-                        {children}
-                    </main>
-                    <Footer></Footer>
-                    <ToastContainer theme="dark" />
-                </body>
-            </html>
+            <GlobalProvider>
+                <html>
+                    <body>
+                        <Navbar></Navbar>
+                        <main>
+                            {children}
+                        </main>
+                        <Footer></Footer>
+                        <ToastContainer theme="dark" />
+                    </body>
+                </html>
+            </GlobalProvider>
         </AuthProvider>
     );
 }
