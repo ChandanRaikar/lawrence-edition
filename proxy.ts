62225308d6fa296ebf85893a23e3538739aa3a62
Chandan <chandan.raikar@live.com>
2026-09-23 12:32:36 +0530

    Added Featured Property feature

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,5 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
   return NextResponse.next();
 }
